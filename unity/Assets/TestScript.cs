fe1c495fba2e0acd053e17c8224f68535aa697d8
Heath Naylor <heath@bythepixel.com>
2017-02-20 22:20:32 -0700

    Continued implementation of perlin

--- unity/Assets/TestScript.cs
+++ unity/Assets/TestScript.cs
@@ -6,7 +6,9 @@ public class TestScript : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		
+		Debug.Log ("test");
+		var osn = new OpenSimplexNoise(2);
+		Debug.Log(osn.Evaluate(1,1));
 	}
 	
 	// Update is called once per frame
