7f81fd4225e8cc8d7ed2b131cab20e75bded6bd6
Roeland Reyniers <roeland@bythepixel.com>
2017-03-20 22:15:54 -0600

    Styling preference

--- database/migrations/20170309044609_add_users_table.php
+++ database/migrations/20170309044609_add_users_table.php
@@ -8,12 +8,13 @@ class AddUsersTable extends AbstractMigration
     {
         $users = $this->table('users');
 
-        $users->addColumn('email', 'string', ['limit' => 255])
-            ->addColumn('password_hash', 'string', ['limit' => 255])
-            ->addColumn('first_name', 'string', ['limit' => 255])
-            ->addColumn('last_name', 'string', ['limit' => 255])
-            ->addColumn('role', 'string', ['limit' => 255])
-            ->addIndex('email', ['unique' => true])
-            ->create();
+        $users->addColumn('email', 'string', ['limit' => 255]);
+        $users->addColumn('password_hash', 'string', ['limit' => 255]);
+        $users->addColumn('first_name', 'string', ['limit' => 255]);
+        $users->addColumn('last_name', 'string', ['limit' => 255]);
+        $users->addColumn('role', 'string', ['limit' => 255]);
+        $users->addIndex('email', ['unique' => true]);
+
+        $users->create();
     }
 }
