4119f5a78b49acb3ff3925d499f8d6be3ca7c5e7
Randen Kelly <randen@appthis.com>
2017-04-11 21:11:10 -0600

    Major updates to elementifier including better validation and performance

--- Elementifier/index.php
+++ Elementifier/index.php
@@ -1,12 +1,12 @@
 <?php
-
 class Elementifier {
-	public $constants, $vowels;
+	public $charsAvail;
+	public static $chars, $realElements, $suffixes, $charFreqArr;
 
 	public function __construct() {
 
 		// letter => frequency (less 1-10 more)
-		$this->consonants = [
+		static::$chars['consonant'] = [
 			'b' => 2,
 			'c'	=> 3,
 			'd'	=> 4,
@@ -20,10 +20,13 @@ class Elementifier {
 			'n' => 7,
 			'p' => 2,
 			'q' => [
-				'weight' => 1,
+				'freq' => 1,
 				'rules' => [
-					0 => function ($prev, $curr) {
-						return $curr.'u';
+					0 => function ($iChar, $currChar, $wordChars) {
+						// ensure a Q is trailed by a U
+						if ($wordChars[$iChar+1] == 'u') {
+							throw new CharValidationException("Consonant rule failed: 'Q is trailed by a U'");
+						}
 					}
 				]
 			],
@@ -35,17 +38,29 @@ class Elementifier {
 			'x' => 1,
 			'z' => 1
 		];
+		static::$charFreqArr['consonant'] = static::generateCharFreqArr(static::$chars, 'consonant');
 
-		$this->vowels = [
+		static::$chars['vowel'] = [
 			'a' => 8,
-			'e' => 10,
+			'e' => [
+				'freq' => 10,
+				'rules' => [
+					0 => function ($iChar, $currChar, $wordChars) {
+						// ensure I before E except after C
+						if ($wordChars[$iChar-1] != 'c' && $wordChars[$iChar+1] == 'i') {
+							throw new CharValidationException("Vowel rule failed: 'I before E'");
+						}
+					}
+				]
+			],
 			'i'	=> 7,
 			'o'	=> 8,
 			'u' => 3,
 			'y' => 2
 		];
+		static::$charFreqArr['vowel'] = static::generateCharFreqArr(static::$chars, 'vowel');
 
-		$this->realElements = [
+		static::$realElements = [
 			'Aluminum',
 			'Copper',
 			'Gold',
@@ -57,7 +72,7 @@ class Elementifier {
 			'Zinc'
 		];
 
-		$this->suffixes = [
+		static::$suffixes = [
 			'ium',
 			'um',
 			'er',
@@ -70,22 +85,22 @@ class Elementifier {
 	}
 
 	private function getSuffix() {
-		$i = rand(0, count($this->suffixes)-1);
-		return $this->suffixes[$i];
+		$i = rand(0, count(static::$suffixes)-1);
+		return static::$suffixes[$i];
 	}
 
 	public function getRoot($prefix, $suffix) {
+		// $likelyhoodToBeginInVowel = 0.125;
+		// $likelyhoodToEndInVowel = 0.75;
+
+		// chose a random character length for the root among the available lengths
 		$lengthRange = [2,3,4];
-
-		$likelyhoodToBeginInVowel = 0.125;
-		$likelyhoodToEndInVowel = 0.75;
-
 		$chosenLengthIdx = rand(0, count($lengthRange) - 1);
 		$chosenLength = $lengthRange[$chosenLengthIdx];
 
 		// chose whether or not to use vowels in the beginning or the end
-		$beginInVowel = $this->randDecimal() >= $likelyhoodToBeginInVowel;
-		$endInVowel = $this->randDecimal() >= $likelyhoodToEndInVowel;
+		// $beginInVowel = static::randDecimal() >= $likelyhoodToBeginInVowel;
+		// $endInVowel = static::randDecimal() >= $likelyhoodToEndInVowel;
 
 		$prevChar = false;	
 
@@ -102,9 +117,9 @@ class Elementifier {
 				: $chosenCharacters[$i-1];
 
 			// make sure two vowels don't exist in a row
-			$chosenChar = $this->isVowel($prevChar)
-				? $this->randConsonant($prevChar)
-				: $this->randVowel();
+			$chosenChar = static::isVowel($prevChar)
+				? static::randConsonant($prevChar)
+				: static::randVowel($prevChar);
 
 			// if a duplicate is found as the prev. char., go again
 			if ($prevChar == $chosenChar) {
@@ -113,75 +128,96 @@ class Elementifier {
 				$chosenCharacters[$i] = $chosenChar;
 			}
 		}
-		// ensure we end vowel - constant combination
-		$potentialEndChar = $this->isVowel($suffixFirstChar)
-			? $this->randConsonant($prevChar)
-			: $this->randVowel();
+
+		// ensure we end in a vowel - constant combination
+		$potentialEndChar = static::isVowel($suffixFirstChar)
+			? static::randConsonant($prevChar)
+			: static::randVowel($prevChar);
 
 		// inject a vowel before the last two consonants
-		$chosenCharacters[] = $this->isVowel($prevChar)
-			? $this->randVowel().$potentialEndChar
+		$chosenCharacters[] = static::isVowel($prevChar)
+			? static::randVowel($prevChar).$potentialEndChar
 			: $potentialEndChar;
 
 		return $chosenCharacters;
 	}
 
-	private function isVowel($char) {
-		return isset($this->vowels[$char]);
-	}
+	// validate the generated word against all rules
+	// if validation passes... well yippee!
+	// else fails... try again
+	private function validateWord($word) {
+		$wordChars = explode('', $word);
 
-	private function randVowel() {
-		if (!isset($this->vowelsAvail)) {
-			$this->vowelsAvail = []; // generate a temp array of vowels with the required frequency for each
-			foreach ($this->vowels as $vowel => $vowelWeight) {
-				for ($i = 0; $i < $vowelWeight; $i++) {
-					$this->vowelsAvail[] = $vowel;
+		foreach ($wordChars as $iChar => $currChar) {
+			$charType = static::isVowel($currChar)
+				? 'vowel'
+				: 'constant';
+
+			// execute rules (callbacks) if available
+			if (!empty(static::$chars[$charType][$currChar]['rules'])) {
+				try {
+					foreach (static::$chars[$charType][$currChar]['rules'] as $ruleId => $callback) {
+						call_user_func_array($callback, [$iChar, $currChar, $wordChars]);
+					}
+				} catch (CharValidationException $e) {
+					echo $e->getMessage();
 				}
 			}
 		}
-
-		$i = rand(0, count($this->vowelsAvail) - 1);
-		return $this->vowelsAvail[$i];
+		
 	}
 
-	private function randConsonant($prev) {
-		if (!isset($this->consonantsAvail)) {
-			$this->consonantsAvail = []; // generate a temp array of consonants with the required frequency for each
-			foreach ($this->consonants as $consonant => $consonantSpec) {
-				$consonantWeight = !is_array($consonantSpec)
-					? $consonantSpec
-					: $consonantSpec['weight'];
+	private static function isVowel($char) {
+		return isset(static::$chars['vowel'][$char]);
+	}
 
-				for ($i = 0; $i < $consonantWeight; $i++) {
-					$this->consonantsAvail[] = $consonant;
-				}
+	private static function randConsonant($prevChar) {
+		return static::randChar('consonant', $prevChar);
+	}
 
+	private static function randVowel($prevChar) {
+		return static::randChar('vowel', $prevChar);
+	}
+
+	private static function generateCharFreqArr($charArr, $type) {
+		$charFreqArr = [];
+		foreach ($charArr[$type] as $char => $charSpec) {
+			$charFreq = !is_array($charSpec)
+				? $charSpec
+				: $charSpec['freq'];
+
+			for ($i = 0; $i < $charFreq; $i++) {
+				$charFreqArr[] = $char;
 			}
+
 		}
 
-		$i = rand(0, count($this->consonantsAvail) - 1);
-		$chosenChar = $this->consonantsAvail[$i];
+		return $charFreqArr;
+	}
 
-		// execute rules (callbacks) if available
-		if (!empty($this->consonants[$chosenChar]['rules'])) {
-			foreach ($this->consonants[$chosenChar]['rules'] as $ruleId => $callback) {
-				$chosenChar = call_user_func_array($callback, [$prev, $this->consonants[$i]]);
-			}
+	private static function randChar($charType, $prev) {
+		
+		if (!isset(static::$charFreqArr[$charType])) {
+			throw new DataMissingException("Missing frequency array for $charType");
 		}
+
+		// grab a random char from the frequency array
+		$i = rand(0, count(static::$charFreqArr[$charType]) - 1);
+		$chosenChar = static::$charFreqArr[$charType][$i];
 		
 		return $chosenChar;
 	}
 
-	private function randDecimal($min = 0, $max = 1) {
+	private static function randDecimal($min = 0, $max = 1) {
 		return rand ($min*10, $max*10) / 10;
 	}
 
 	private function randomKey(array $array)
 	{
-	    return $this->randomElement(array_keys($array));
+	    return static::randomElement(array_keys($array));
 	}
 
-	private function randomElement(array $array)
+	private static function randomElement(array $array)
 	{
 	    if (count($array) === 0)
 	    {
@@ -195,13 +231,19 @@ class Elementifier {
 	    return $array[$array_keys[$rand]];
 	}
 
-	public function generateWordsForElements() {
+	public static function grabElementPrefix($element, $chars = 4) {
+		$prefixArr = str_split($element, $chars);
+		return ucfirst($prefixArr[0]);
+	}
+
+	public function generateWordsForElements($count) {
 		$generated = [];
-		foreach ($this->realElements as $element) {
-			$prefixArr = str_split($element, 4);
-			$prefix = ucfirst($prefixArr[0]);
-			for ($i=0;$i<10;$i++){
+		foreach (static::$realElements as $element) {
+			
+			$prefix = static::grabElementPrefix($element);
+			for ($i=0; $i<$count; $i++){
 				$suffix = $this->getSuffix();
+			
 				$generated[$element][$i] = $prefix.implode('',$this->getRoot($prefix, $suffix)).$suffix;
 			}
 		}
@@ -209,5 +251,12 @@ class Elementifier {
 	}
 }
 
+class CharValidationException extends Exception {
+
+}
+class DataMissingException extends Exception {
+
+}
+
 $elementifier = new Elementifier();
-var_dump($elementifier->generateWordsForElements());
+var_dump($elementifier->generateWordsForElements(10));
